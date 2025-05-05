d7f3f73d6ce6cef866da4bf9ae4b343abe2d9c44
Albert Hupa <albert.hupa@lingarogroup.com>
2025-05-05 14:02:28 +0200

    cosmos 2

--- docs/azure_cosmos_db.py
+++ docs/azure_cosmos_db.py
@@ -171,11 +171,17 @@ vector_embedding_policy = {
     ]
 }
 
+full_text_policy = {
+    "defaultLanguage": "en-US",
+    "fullTextPaths": [{"path": "/text", "language": "en-US"}],
+}
+
 indexing_policy = {
     "indexingMode": "consistent",
     "includedPaths": [{"path": "/*"}],
     "excludedPaths": [{"path": '/"_etag"/?'}],
     "vectorIndexes": [{"path": "/embedding", "type": "diskANN"}],
+    "fullTextIndexes": [{"path": "/text"}],
 }
 
 
